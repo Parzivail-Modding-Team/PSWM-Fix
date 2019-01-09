7f8acd76b49b1dddc2f109bae85f2bb0360c21c6
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-01-09 17:27:47 +0100

    Initial commit with mod skeleton

---
+++ src/main/java/com/github/kaeptmblaubaer1000/pswmfix/PSWMFix.java
@@ -0,0 +1,21 @@
+package com.github.kaeptmblaubaer1000.pswmfix;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import java.io.File;
+import java.util.Random;
+
+@Mod(modid = "pswmfix", version = "1.4.0", dependencies = "required-after:starwarsmod")
+public class StarWarsGalaxy
+{
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		;
+	}
+}
