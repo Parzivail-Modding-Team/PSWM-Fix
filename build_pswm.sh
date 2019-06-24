0d8dded005b61d2169ef04241547a3313edd8635
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-06-24 07:46:27 +0200

    Fix dependency on PSWM

---
+++ build_pswm.sh
@@ -0,0 +1,6 @@
+(
+cd PSWM
+bash ./gradlew jar
+)
+mkdir -p libraries/com/parzivail/starwarsmod/pswm/1.3.3
+cp PSWM/build/libs/starwarsmod-1.3.3.jar libraries/com/parzivail/starwarsmod/pswm/1.3.3/pswm-1.3.3-dev.jar
