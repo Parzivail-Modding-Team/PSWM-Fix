1d7e6f0b9447d9ccc4d78c7469f431fb120484e4
kb1000 <kaeptmblaubaer1000@gmail.com>
2019-06-26 16:07:19 +0200

    Make first version that is loadable by Forge

--- src/main/java/com/github/kb1000/pswmfix/PSWMFix.java
+++ src/main/java/com/github/kb1000/pswmfix/PSWMFix.java
@@ -1,21 +1,26 @@
-package com.github.kaeptmblaubaer1000.pswmfix;
+package com.github.kb1000.pswmfix;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.item.ItemStack;
+
+import com.parzivail.pswm.StarWarsItems;
+import com.parzivail.pswm.StarWarsMod;
 
 import java.io.File;
 import java.util.Random;
 
 @Mod(modid = "pswmfix", version = "1.4.0", dependencies = "required-after:starwarsmod")
-public class StarWarsGalaxy
+public class PSWMFix
 {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event)
 	{
-		;
+		GameRegistry.addSmelting(StarWarsMod.blockCrystalOre, new ItemStack(StarWarsItems.lightsaberCrystal, 1), 0.2F);
 	}
 }
