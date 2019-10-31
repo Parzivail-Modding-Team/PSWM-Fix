8b159b956e087c735c6352f6c3af5c9309d11d43
kb1000 <kaeptmblaubaer1000@gmail.com>
2019-10-31 16:07:38 +0100

    Make static NPC blocks indestructible by explosions

--- src/main/java/com/github/kb1000/pswmfix/PSWMFix.java
+++ src/main/java/com/github/kb1000/pswmfix/PSWMFix.java
@@ -29,5 +29,25 @@ public class PSWMFix
 				}
 			}
 		}
+		StarWarsMod.blockStaticNpcBartender.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcCorellian.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcEmpireEndorQuartermaster.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcImperialCody.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcImperialDaala.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcImperialFurgan.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcImperialVeers.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcJawaQuartermaster.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcMerchant.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcObiWan.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRebelCarlist.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRebelDreis.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRebelRex.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRebelTantor.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRebelYavinQuartermaster.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRecruiterEmpire.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcRecruiterRebel.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcSith.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcWeaponsDealer.setBlockUnbreakable().setResistance(6000000.0F);
+		StarWarsMod.blockStaticNpcYoda.setBlockUnbreakable().setResistance(6000000.0F);
 	}
 }
